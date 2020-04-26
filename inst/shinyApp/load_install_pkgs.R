ccc30e3b93971e256f1e210377928e42084a3268
PJoserAtauchi <pjoseratauchi@MacBook-Pro.local>
2020-04-25 21:53:17 -0500

    v0

--- inst/shinyApp/load_install_pkgs.R
+++ inst/shinyApp/load_install_pkgs.R
@@ -1,4 +1,4 @@
-pkg_check <- c("shiny","ggplot2","shinyBS","dplyr","shinysky","devtools")
+pkg_check <- c("shiny","ggplot2","shinyBS","dplyr","shinysky","devtools","iNEXT")
 
 pkgs_ntb_miss <- pkg_check[!(pkg_check %in% installed.packages())]
 
